a9aa6845ce8bd5066662de5c58f0ab2e07b97d2b
AmrMohammed0 <144250969+AmrMohammed0@users.noreply.github.com>
2024-03-30 10:57:53 +0200

    Update email_field.blade.php

--- resources/views/web/default/auth/register_includes/email_field.blade.php
+++ resources/views/web/default/auth/register_includes/email_field.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group">
-    <label class="input-label" for="email">{{ trans('auth.email') }} {{ !empty($optional) ? "(". trans('public.optional') .")" : '' }}:</label>
+    <label class="input-label" for="email">{{ trans('auth.email') }} {{ !empty($optional) ? "(". trans('public.optional') .")" : '' }}*</label>
     <input name="email" type="text" class="form-control @error('email') is-invalid @enderror"
            value="{{ old('email') }}" id="email" aria-describedby="emailHelp">
 
