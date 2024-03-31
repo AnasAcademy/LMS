7db8f134b7ec17038a733aaeae8b15fcc15fee1c
AmrMohammed0 <144250969+AmrMohammed0@users.noreply.github.com>
2024-03-31 20:15:21 +0200

    Update navbar

--- resources/views/web/default/includes/navbar.blade.php
+++ resources/views/web/default/includes/navbar.blade.php
@@ -18,13 +18,26 @@
         }
     }
 @endphp
+@php
+    $userLanguages = !empty($generalSettings['site_language']) ? [$generalSettings['site_language'] => getLanguages($generalSettings['site_language'])] : [];
 
+    if (!empty($generalSettings['user_languages']) and is_array($generalSettings['user_languages'])) {
+        $userLanguages = getLanguages($generalSettings['user_languages']);
+    }
+
+    $localLanguage = [];
+
+    foreach($userLanguages as $key => $userLanguage) {
+        $localLanguage[localeToCountryCode($key)] = $userLanguage;
+    }
+
+@endphp
 <div id="navbarVacuum"></div>
 <nav id="navbar" class="navbar navbar-expand-lg navbar-light">
     <div class="{{ (!empty($isPanel) and $isPanel) ? 'container-fluid' : 'container'}}">
         <div class="d-flex align-items-center justify-content-between w-100">
 
-            <a class="navbar-brand navbar-order d-flex align-items-center justify-content-center mr-0 {{ (empty($navBtnUrl) and empty($navBtnText)) ? 'ml-auto' : '' }}" href="/">
+            <a class="navbar-brand navbar-order d-flex align-items-center justify-content-center mr-0 {{ (empty($navBtnUrl) and empty($navBtnText)) ? 'mr-auto' : '' }}" href="http://anasacademy.uk/">
                 @if(!empty($generalSettings['logo']))
                     <img src="{{ $generalSettings['logo'] }}" class="img-cover" alt="site logo">
                 @endif
@@ -60,7 +73,7 @@
                                                         </div>
 
                                                         @if(!empty($category->subCategories) and count($category->subCategories))
-                                                            <i data-feather="chevron-right" width="20" height="20" class="d-none d-lg-inline-block ml-10"></i>
+                                                            <i data-feather="chevron-right" width="20" height="20" class="d-none d-lg-inline-block mr-10"></i>
                                                             <i data-feather="chevron-down" width="20" height="20" class="d-inline-block d-lg-none"></i>
                                                         @endif
                                                     </a>
@@ -121,9 +134,66 @@
 
             </div>
         </div>
+         <div class="d-flex align-items-center justify-content-between justify-content-md-center">
+
+                {{-- Currency --}}
+                @include('web.default.includes.top_nav.currency')
+
+
+                @if(!empty($localLanguage) and count($localLanguage) > 1)
+                    <form action="/locale" method="post" class="mr-15 mx-md-20">
+                        {{ csrf_field() }}
+
+                        <input type="hidden" name="locale">
+
+                        @if(!empty($previousUrl))
+                            <input type="hidden" name="previous_url" value="{{ $previousUrl }}">
+                        @endif
+
+                        <div class="language-select">
+                            <div id="localItems"
+                                 data-selected-country="{{ localeToCountryCode(mb_strtoupper(app()->getLocale())) }}"
+                                 data-countries='{{ json_encode($localLanguage) }}'
+                            ></div>
+                        </div>
+                    </form>
+                @else
+                    <div class="mr-15 mx-md-20"></div>
+                @endif
+
+
+                {{-- <form action="/search" method="get" class="form-inline my-2 my-lg-0 navbar-search position-relative">
+                    <input class="form-control mr-5 rounded" type="text" name="search" placeholder="{{ trans('navbar.search_anything') }}" aria-label="Search">
+
+                    <button type="submit" class="btn-transparent d-flex align-items-center justify-content-center search-icon">
+                        <i data-feather="search" width="20" height="20" class="mr-10"></i>
+                    </button>
+                </form> --}}
+            </div>
+        </div>
+
+        <div class="xs-w-100 d-flex align-items-center justify-content-between ">
+            <div class="d-flex">
+
+                {{-- @include(getTemplate().'.includes.shopping-cart-dropdwon') --}}
+
+                <div class="border-left mx-5 mx-lg-15"></div>
+
+              {{--  @include(getTemplate().'.includes.notification-dropdown')--}}
+            </div>
+
+            {{-- User Menu --}}
+            @include('web.default.includes.top_nav.user_menu')
+        </div>
     </div>
+    </div>
+    
 </nav>
 
 @push('scripts_bottom')
     <script src="/assets/default/js/parts/navbar.min.js"></script>
+      <link href="/assets/default/vendors/flagstrap/css/flags.css" rel="stylesheet">
+    <script src="/assets/default/vendors/flagstrap/js/jquery.flagstrap.min.js"></script>
+    <script src="/assets/default/js/parts/top_nav_flags.min.js"></script>
 @endpush
+
