fbd6804216741ab1b6aea004f7e2145a9abab4dc
AmrMohammed0 <144250969+AmrMohammed0@users.noreply.github.com>
2024-04-06 22:28:47 +0200

    add titles

--- resources/views/web/default/panel/includes/downloadfiles.blade.php
+++ resources/views/web/default/panel/includes/downloadfiles.blade.php
@@ -127,30 +127,44 @@
             <div class="product-file-type">
                 <ul class="list-unstyled">
                     <li class="media mb-3">
-                        <span class="ml-3 align-self-center img-icon primary-rgba text-primary">.doc</span>
+                        <span class="ml-3 align-self-center img-icon danger-rgba text-danger">.pdf</span>
                         <div class="media-body">
-                        <h5 class="font-16 mb-1">Marketing Guidelines<i class="feather icon-download-cloud float-right"></i></h5>
-                        <p>.pdf, 10.5 MB</p>
-                        </div>
-                    </li>
-                    <li class="media mb-3">
-                        <span class="ml-3 align-self-center img-icon success-rgba text-success">.xls</span>
-                        <div class="media-body">
-                        <h5 class="font-16 mb-1">Complete Product Sheet<i class="feather icon-download-cloud float-right"></i></h5>
+                        <h5 class="font-16 mb-1">Microsoft Teams Arabic<i class="feather icon-download-cloud float-right"></i></h5>
                         <p>.pdf, 10.5 MB</p>
                         </div>
                     </li>
                     <li class="media mb-3">
                         <span class="ml-3 align-self-center img-icon danger-rgba text-danger">.pdf</span>
                         <div class="media-body">
-                        <h5 class="font-16 mb-1">Annual Sales Report 2018-19<i class="feather icon-download-cloud float-right"></i></h5>
+                        <h5 class="font-16 mb-1">Microsoft Teams English<i class="feather icon-download-cloud float-right"></i></h5>
+                        <p>.pdf, 10.5 MB</p>
+                        </div>
+                    </li>
+                    <li class="media mb-3">
+                        <span class="ml-3 align-self-center img-icon danger-rgba text-danger">.pdf</span>
+                        <div class="media-body">
+                        <h5 class="font-16 mb-1">Outlook Arabic<i class="feather icon-download-cloud float-right"></i></h5>
                         <p>.pdf, 10.5 MB</p>
                         </div>
                     </li>
                     <li class="media">
-                        <span class="ml-3 align-self-center img-icon info-rgba text-info">.zip</span>
+                        <span class="ml-3 align-self-center img-icon danger-rgba text-danger">.pdf</span>
                         <div class="media-body">
-                        <h5 class="font-16 mb-1">Brand Photography<i class="feather icon-download-cloud float-right"></i></h5>
+                        <h5 class="font-16 mb-1">Outlook English<i class="feather icon-download-cloud float-right"></i></h5>
+                        <p>.pdf, 10.5 MB</p>
+                        </div>
+                    </li>
+                     <li class="media">
+                        <span class="ml-3 align-self-center img-icon danger-rgba text-danger">.pdf</span>
+                        <div class="media-body">
+                        <h5 class="font-16 mb-1">OneDrive English<i class="feather icon-download-cloud float-right"></i></h5>
+                        <p>.pdf, 10.5 MB</p>
+                        </div>
+                    </li>
+                     <li class="media">
+                        <span class="ml-3 align-self-center img-icon danger-rgba text-danger">.pdf</span>
+                        <div class="media-body">
+                        <h5 class="font-16 mb-1">OneDrive Arabic<i class="feather icon-download-cloud float-right"></i></h5>
                         <p>.pdf, 10.5 MB</p>
                         </div>
                     </li>
