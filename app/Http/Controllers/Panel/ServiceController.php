686949ae6ffa67828e702369ebbcae3ecaef7277
EmanMohammed0 <eman.m@anasacademy.uk>
2024-05-02 14:38:46 +0300

    return only the active service

--- app/Http/Controllers/Panel/ServiceController.php
+++ app/Http/Controllers/Panel/ServiceController.php
@@ -16,7 +16,7 @@ class ServiceController extends Controller
     public function index()
     {
         //
-        $services = Service::get();
+        $services = Service::where('status', 'active')->get();
         return view(getTemplate() . '.panel.services.index', compact('services'));
     }
 
