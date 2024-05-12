3d7b280e41b96f87ee47efd6043f6014e6609714
NahedFathi0 <nahed@anasacademy.uk>
2024-05-12 15:31:30 +0300

    merge nahed into main

--- routes/admin.php
+++ routes/admin.php
@@ -62,6 +62,7 @@ Route::group(['prefix' => $prefix, 'namespace' => 'Admin', 'middleware' => ['web
         Route::group(['prefix' => 'students'], function () {
             Route::get('/', 'UserController@students');
             Route::get('/registered_users', 'UserController@RegisteredUsers');
+            Route::get('/enrollers', 'UserController@Enrollers');
             Route::get('/users', 'UserController@Users');
             Route::get('/excel', 'UserController@exportExcelUsers');
             Route::get('/excelStudent', 'UserController@exportExcelStudents');
