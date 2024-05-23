cc1f61fd1eded77f1f058e58002ea93f10615eb3
EmanMohammed0 <eman.m@anasacademy.uk>
2024-05-23 17:08:46 +0300

    write user class name correct

--- routes/admin.php
+++ routes/admin.php
@@ -924,7 +924,7 @@ Route::group(['prefix' => $prefix, 'namespace' => 'Admin', 'middleware' => ['web
             Route::get('/{id}/delete', 'BundleController@destroy');
             Route::post('/search', 'BundleController@search');
             Route::get('/excel', 'BundleController@exportExcel');
-            Route::get('/bundleCodeExcel', 'userController@exportBundles');
+            Route::get('/bundleCodeExcel', 'UserController@exportBundles');
 
             Route::get('/{id}/students', 'BundleController@studentsLists');
             Route::get('/{id}/sendNotification', 'BundleController@notificationToStudents');
