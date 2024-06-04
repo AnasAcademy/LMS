37b7a019bfe76d8c356e05b151f951f175038ade
eman-m0hamed <emanmohamed73359@gmail.com>
2024-06-04 14:16:14 +0300

    edit the display of bundles requirements

--- resources/views/web/default/panel/requirements/index.blade.php
+++ resources/views/web/default/panel/requirements/index.blade.php
@@ -20,79 +20,71 @@
     @include('web.default.panel.requirements.requirements_includes.progress')
 
     <section class="row mt-80 mx-0 justify-content-center">
-        @if (count($bundleInstallments) > 0)
+        @if (count($studentBundles) > 0)
             @php
                 $count = 0;
             @endphp
-            @foreach ($bundleInstallments as $bundleId => $bundleData)
+            @foreach ($studentBundles as $studentBundle)
                 @php
                     $count++;
                 @endphp
-
                 <section
                     class="bg-white position-relative col-xl-9 col-12 justify-content-center align-items-center rounded-sm mb-80 py-35 px-0">
                     <h2 class="mb-25 col-12">
-                        {{ clean($bundleData['bundle']->bundle->title, 't') }}</h2>
-
-
-                    @if (empty($bundleData['bundle']->studentRequirement))
-                        @if ($bundleData['bundle']->status == 'pending')
+                        {{ clean($studentBundle->bundle->title, 't') }}</h2>
+                    @if (empty($studentBundle->studentRequirement))
+                     @if ($studentBundle->status == 'pending')
+                        <div class="w-100 text-center">
+                            <p class="alert alert-info text-center mx-30">
+                                طلبك لحجز مقعد دراسي تحت المراجعه من قبل الإدارة المالية يرجي الانتظار حتي يتم مراجعته
+                            </p>
+                        </div>
+                     @elseif ($studentBundle->status == 'fee_rejected')
+                        <div class="w-100 text-center">
+                            <p class="alert alert-danger text-center text-white mx-30">
+                                طلبك لحجز مقعد دراسي تم رفضه من قبل الإدارة المالية لمعرفة السبب اضغط هنا
+                            </p>
+                            <a href="/panel/financial/offline-payments"
+                                class="btn btn-success p-5 mt-20 bg-secondary">للذهاب لمتابعة طلبك اضغط
+                                هنا</a>
+                        </div>
+                        @elseif ($studentBundle->bundle->early_enroll)
                             <div class="w-100 text-center">
                                 <p class="alert alert-info text-center mx-30">
-                                    طلبك لحجز مقعد دراسي تحت المراجعه من قبل الإدارة المالية يرجي الانتظار حتي يتم مراجعته
+                                    يرجى ملاحظة أن التسجيل الرسمي سيبدأ في شهر يوليو المقبل.
+                                    <br> بمجرد فتح التسجيل، ستتمكن من استكمال رفع المتطلبات اللازمة وإتمام إجراءات التسجيل.
                                 </p>
-                            </div>
-                        @elseif ($bundleData['bundle']->status == 'rejected')
-                            <div class="w-100 text-center">
-                                <p class="alert alert-danger text-center text-white mx-30">
-                                    طلبك لحجز مقعد دراسي تم رفضه من قبل الإدارة المالية لمعرفة السبب اضغط هنا
-                                </p>
-                                <a href="/panel/financial/offline-payments"
-                                    class="btn btn-success p-5 mt-20 bg-secondary">للذهاب لمتابعة طلبك اضغط
-                                    هنا</a>
                             </div>
                         @else
-                            @if ($bundleData['bundle']->upload_later == true)
-                                @include('web.default.panel.requirements.payment_card', [
-                                    'bundleData' => $bundleData,
-                                ])
-                            @elseif ($bundleData['bundle']->bundle->early_enroll)
-                                <div class="w-100 text-center">
-                                    <p class="alert alert-info text-center mx-30">
-                                        يرجى ملاحظة أن التسجيل الرسمي سيبدأ في شهر يوليو المقبل.
-                                        <br> بمجرد فتح التسجيل، ستتمكن من استكمال رفع المتطلبات اللازمة وإتمام إجراءات
-                                        التسجيل.
-                                    </p>
-                                </div>
-                            @else
-                                <div class="w-100 text-center">
-                                    <p class="alert alert-info text-center mx-30">
-                                        لم يتم رفع متطلبات القبول بعد ، يرجي الضعط علي الزر للذهاب لصفحة متطلبات القبول
-                                    </p>
-                                    <a href="/panel/bundles/{{ $bundleData['bundle']->id }}/requirements"
-                                        class="btn btn-success p-5 mt-20 bg-secondary">للذهاب لرفع ملفات متطلبات القبول اضغط
-                                        هنا</a>
-                                </div>
-                            @endif
+                            <div class="w-100 text-center">
+                                <p class="alert alert-info text-center mx-30">
+                                    لم يتم رفع متطلبات القبول بعد ، يرجي الضعط علي الزر للذهاب لصفحة متطلبات القبول
+                                </p>
+                                <a href="/panel/bundles/{{ $studentBundle->id }}/requirements"
+                                    class="btn btn-success p-5 mt-20 bg-secondary">للذهاب لرفع ملفات متطلبات القبول اضغط
+                                    هنا</a>
+                            </div>
                         @endif
                     @else
-                        @if ($bundleData['bundle']->studentRequirement->status == 'pending')
+                        @if ($studentBundle->studentRequirement->status == 'pending')
                             <div class="w-100 text-center">
                                 <p class="alert alert-info text-center mx-30">
                                     لقد تم بالفعل رفع متطلبات القبول يرجي الانتظار حتي يتم مراجعتها
                                 </p>
                             </div>
-                        @elseif ($bundleData['bundle']->studentRequirement->status == 'approved')
-                            @include('web.default.panel.requirements.payment_card', [
-                                'bundleData' => $bundleData,
-                            ])
-                        @elseif ($bundleData['bundle']->studentRequirement->status == 'rejected')
-                            <div class="w-100 text-center mx-30">
-                                <p class="alert alert-danger text-center text-white">
+                        @elseif ($studentBundle->studentRequirement->status == 'approved')
+                        <div class="w-100 text-center">
+                            <p class="alert alert-success text-center mx-30">
+                                لقد تم بالفعل رفع متطلبات القبول وتم الموافقة عليها
+                            </p>
+                        </div>
+                        @elseif ($studentBundle->studentRequirement->status == 'rejected')
+                            <div class="w-100 text-center">
+                                <p class="alert alert-danger text-center text-white mx-30">
                                     لقد تم رفض الملفات التي قمت برفعها يرجي مراجعة الميل لمشاهدة السبب ثم ارفع الملفات مرة
                                     اخري
                                 </p>
-                                <a href="/panel/bundles/{{ $bundleData['bundle']->id }}/requirements"
+                                <a href="/panel/bundles/{{ $studentBundle->id }}/requirements"
                                     class="btn btn-primary p-5 mt-20">للذهاب
                                     لرفع الملفات مرة اخري اضغط هنا</a>
                             </div>
