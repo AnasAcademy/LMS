8d2d076b4e7e7c0726043841fb493ad3a2c6710b
NahedFathi0 <nahed@anasacademy.uk>
2024-06-13 11:21:22 +0300

    fix order id error

--- app/Http/Controllers/Web/PaymentController.php
+++ app/Http/Controllers/Web/PaymentController.php
@@ -440,7 +440,6 @@ class PaymentController extends Controller
                         'requirement_endorsement'
                     ];
                     $studentData = collect($userData)->except($keysToExclude)->toArray();
-                    $user = auth()->user();
                     $student = $user->student;
 
 
@@ -496,7 +495,7 @@ class PaymentController extends Controller
                         ]);
                     }
                 } catch (\Exception $exception) {
-                    dd($userData);
+                    dd([$userData,$exception]);
                 }
             } elseif ($bundle_sale && $bundle_sale->order->user_id == $user->id && $bundle_sale->order->status == 'paid') {
                 $user = User::where('id', $user->id)->first();
