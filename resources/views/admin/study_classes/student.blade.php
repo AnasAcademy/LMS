c43e0217cef3e60412e46f0752953c54b63d0d75
EmanMohammed0 <eman.m@anasacademy.uk>
2024-07-22 12:10:58 +0300

    display student count

--- resources/views/admin/study_classes/student.blade.php
+++ resources/views/admin/study_classes/student.blade.php
@@ -15,57 +15,77 @@
         </div>
     </section>
 
-
-    <section class="card">
-        <div class="card-body">
-            <form method="get" class="mb-0">
-
-                <div class="row">
-                    <div class="col-md-3">
-                        <div class="form-group">
-                            <label class="input-label">كود الطالب</label>
-                            <input name="user_code" type="text" class="form-control"
-                                value="{{ request()->get('user_code') }}">
-                        </div>
+    <div class="section-body">
+        <div class="row">
+            <div class="col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-primary">
+                        <i class="fas fa-users"></i>
                     </div>
-
-                    <div class="col-md-3">
-                        <div class="form-group">
-                            <label class="input-label">بريد الطالب</label>
-                            <input name="email" type="text" class="form-control" value="{{ request()->get('email') }}">
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>{{ trans('admin/main.total_students') }}</h4>
                         </div>
-                    </div>
-
-
-                    <div class="col-md-3">
-                        <div class="form-group">
-                            <label class="input-label">اسم الطالب</label>
-                            <input
-                                name={{ request()->is(getAdminPanelUrl('/students/users', false)) ? 'ar_name' : 'full_name' }}
-                                type="text" class="form-control"
-                                value="{{ request()->get('ar_name') }}{{ request()->get('full_name') }}">
-                        </div>
-                    </div>
-
-                    <div class="col-md-3">
-                        <div class="form-group">
-                            <label class="input-label">هاتف الطالب</label>
-                            <input name="mobile" type="text" class="form-control"
-                                value="{{ request()->get('mobile') }}">
-                        </div>
-                    </div>
-
-                    <div class="col-md-3">
-                        <div class="form-group mt-1">
-                            <label class="input-label mb-4"> </label>
-                            <input type="submit" class="text-center btn btn-primary w-100"
-                                value="{{ trans('admin/main.show_results') }}">
+                        <div class="card-body">
+                            {{ $enrollments->count() }}
                         </div>
                     </div>
                 </div>
-            </form>
+            </div>
         </div>
-    </section>
+
+        <section class="card">
+            <div class="card-body">
+                <form method="get" class="mb-0">
+
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label class="input-label">كود الطالب</label>
+                                <input name="user_code" type="text" class="form-control"
+                                    value="{{ request()->get('user_code') }}">
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label class="input-label">بريد الطالب</label>
+                                <input name="email" type="text" class="form-control"
+                                    value="{{ request()->get('email') }}">
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label class="input-label">اسم الطالب</label>
+                                <input
+                                    name={{ request()->is(getAdminPanelUrl('/students/users', false)) ? 'ar_name' : 'full_name' }}
+                                    type="text" class="form-control"
+                                    value="{{ request()->get('ar_name') }}{{ request()->get('full_name') }}">
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label class="input-label">هاتف الطالب</label>
+                                <input name="mobile" type="text" class="form-control"
+                                    value="{{ request()->get('mobile') }}">
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <div class="form-group mt-1">
+                                <label class="input-label mb-4"> </label>
+                                <input type="submit" class="text-center btn btn-primary w-100"
+                                    value="{{ trans('admin/main.show_results') }}">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+    </div>
 
 
     <div class="card">
@@ -124,18 +144,17 @@
 
                             <td>
                                 @foreach ($enrollment->student->classBundleStudent($class->id) as $record)
-
                                     {{ $record->bundle->title }}
                                     @if (!$loop->last)
                                         و
-                                        @endif
+                                    @endif
                                 @endforeach
                             </td>
                             <td>
                                 @foreach ($enrollment->student->classBundleStudent($class->id) as $record)
                                     {{ dateTimeFormat(strtotime($enrollment->created_at), 'j M Y | H:i') }}
                                     @if (!$loop->last)
-                                      و
+                                        و
                                     @endif
                                 @endforeach
                             </td>
