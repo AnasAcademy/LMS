a947fc2669efd0b6394cb72dba19affaf8e2ecd1
EmanMohammed0 <eman.m@anasacademy.uk>
2024-07-22 14:50:17 +0300

    edit the function of enrollments

--- app/Models/StudyClass.php
+++ app/Models/StudyClass.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\BundleStudent;
+use App\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,8 +13,16 @@ class StudyClass extends Model
     protected $guarded=[];
 
     public function enrollments(){
-        return $this->hasMany(BundleStudent::class, 'class_id')->groupBy('student_id');
+        return User::whereHas(
+            'bundleSales',
+            function ($query) {
+                $query->where("class_id", $this->id)->groupBy('buyer_id');
+            }
+        )->get();
     }
+    // public function enrollments(){
+    //     return $this->hasMany(BundleStudent::class, 'class_id')->groupBy('student_id');
+    // }
 
     public function sales()
     {
