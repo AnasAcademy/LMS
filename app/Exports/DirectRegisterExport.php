fc0ce7839ead08cd05d449c7c5570ab800747e97
eman-m0hamed <emanmohamed73359@gmail.com>
2024-09-19 15:07:40 +0300

    edit student export to include about us data

--- app/Exports/DirectRegisterExport.php
+++ app/Exports/DirectRegisterExport.php
@@ -42,6 +42,7 @@ class DirectRegisterExport implements FromCollection, WithHeadings, WithMapping
             'created at',
             'Status',
             'Mobile',
+            'about_us'
 
 
         ];
@@ -78,6 +79,7 @@ class DirectRegisterExport implements FromCollection, WithHeadings, WithMapping
                $created_at,
                 $user->status,
                 $user->mobile,
+                $user->student->about_us
             ];
         } else {
             return [
@@ -86,6 +88,7 @@ class DirectRegisterExport implements FromCollection, WithHeadings, WithMapping
                 '',
                 '',
                 'غير مسجل بعد',
+                '',
                 '',
                 '',
                 ''
