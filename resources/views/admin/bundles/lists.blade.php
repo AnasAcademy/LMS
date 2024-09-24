2c6abfdb09a7780d026d1e7bed4d3825fb22bb1c
EmanMohammed0 <eman.m@anasacademy.uk>
2024-09-24 16:15:24 +0300

    edit the url of edit bundle

--- resources/views/admin/bundles/lists.blade.php
+++ resources/views/admin/bundles/lists.blade.php
@@ -332,7 +332,7 @@
                                                         @endcan
 
                                                         @can('admin_bundles_edit')
-                                                            <a href="{{ getAdminPanelUrl() }}/bundles/{{ $bundle->id }}/edit" target="_blank" class="d-flex align-items-center text-dark text-decoration-none btn-transparent btn-sm text-primary mt-1 " title="{{ trans('admin/main.edit') }}">
+                                                            <a href="{{ getAdminPanelUrl() }}/bundles/{{ $bundle->id }}/edit?type={{ $bundle->type }}" target="_blank" class="d-flex align-items-center text-dark text-decoration-none btn-transparent btn-sm text-primary mt-1 " title="{{ trans('admin/main.edit') }}">
                                                                 <i class="fa fa-edit"></i>
                                                                 <span class="ml-2">{{ trans('admin/main.edit') }}</span>
                                                             </a>
